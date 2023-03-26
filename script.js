f99681f6de7f8b75b15fb23f220aef0bd52b626e
Lukas <lukasasaelbacelar@gmail.com>
2023-03-26 19:05:42 -0300

    etch a sketch 16x16 implemented

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-function generateDivs() {
+let gridsize = 16;
+const container = document.querySelector("#container");
+
+function generateDivs(gridsize) {
     const container = document.querySelector('#container');
 
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-            const pixel = document.createElement('div');
+    for (let i = 0; i < gridsize; i++) {
+        for (let j = 0; j < gridsize; j++) {
+            const pixel = document.createElement('btn');
             pixel.classList.add('pixel');
-            pixel.textContent = j;
+            pixel.addEventListener('mouseover', function handleClick(event) {
+                pixel.style.backgroundColor = "grey";
+            });
             container.appendChild(pixel);
         }
     }
-};
+}
 
-generateDivs();
+function resetBtn() {
+    const pixels = document.querySelectorAll("btn");
+    for (let i = 0; i < pixels.length; i++) {
+        pixels[i].style.backgroundColor = "white";
+    };
+}
+
+function setGridSize16() {
+    let gridsize = 16;
+    while (container.firstChild)
+        container.removeChild(container.firstChild);
+    generateDivs(gridsize);
+}
+
+function setGridSize32() {
+    let gridsize = 32;
+    while (container.firstChild)
+        container.removeChild(container.firstChild);
+    generateDivs(gridsize);
+}
+
+function setGridSize64() {
+    let gridsize = 64;
+    while (container.firstChild)
+        container.removeChild(container.firstChild);
+    generateDivs(gridsize);
+}
+
+generateDivs(gridsize);
